fc1338ccb7fd224b3d04b4cc6c3be6c1e8160ba2
kilic-burak <burak-kilicc@outlook.com.tr>
2024-12-04 23:01:19 +0300

    Proje updated

--- src/main/java/org/example/models/ResponseItem.java
+++ src/main/java/org/example/models/ResponseItem.java
@@ -1,11 +1,16 @@
 package org.example.models;
 
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ResponseItem{
 	private String date;
+	@JsonProperty("id")
 	private int id;
 	private int userId;
 	private List<ProductsItem> products;
--- src/test/java/helper/ValidatorUsers.java
+++ src/test/java/helper/ValidatorUsers.java
@@ -1,18 +1,35 @@
 package helper;
 
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
+import org.example.models.ProductsItem;
+import org.example.models.ResponseItem;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-
 
 public class ValidatorUsers {
 
 
     public static void validateStatusCode(Response response, int expectedStatus) {
         int actualStatusCode = response.statusCode();
-        assertThat("\nExpected Status Code: "+expectedStatus + "\nActual Status Code: "+actualStatusCode,expectedStatus, is(actualStatusCode));
+        BaseAssertionLogger.logAndAssert(expectedStatus,is(actualStatusCode),"Pass","Fail");
+
+    }
+    public static void validateUsers(ResponseItem actualUsers, ResponseItem expectedUsers){
+
+        int actualId= actualUsers.getId();
+//        int actualUserId=actualUsers.getUserId();
+//        String actualDate = actualUsers.getDate();
+//
+//        for (ProductsItem productsItem:actualUsers.getProducts()){
+//            int productId = productsItem.getProductId();
+//            int quantity = productsItem.getQuantity();
+//        }
+        int expectedId= expectedUsers.getId();
+
+
+        //BaseAssertionLogger.logAndAssert(actualId,expectedId,"pass","fail");
+
     }
 
 }
