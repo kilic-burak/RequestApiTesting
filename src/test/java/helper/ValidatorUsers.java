d3d4393da922aa1d42e950f30d3697b78703bf9b
kilic-burak <burak-kilicc@outlook.com.tr>
2024-12-05 23:18:08 +0300

    ValidatorUsers updated

--- src/test/java/helper/ValidatorUsers.java
+++ src/test/java/helper/ValidatorUsers.java
@@ -5,30 +5,40 @@ import org.example.models.ProductsItem;
 import org.example.models.ResponseItem;
 
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ValidatorUsers {
 
 
     public static void validateStatusCode(Response response, int expectedStatus) {
         int actualStatusCode = response.statusCode();
-        BaseAssertionLogger.logAndAssert(expectedStatus,is(actualStatusCode),"Pass","Fail");
+        BaseAssertionLogger.logAndAssert(expectedStatus,is(actualStatusCode),"Pass","Fail","Status Code");
 
     }
     public static void validateUsers(ResponseItem actualUsers, ResponseItem expectedUsers){
 
         int actualId= actualUsers.getId();
-//        int actualUserId=actualUsers.getUserId();
-//        String actualDate = actualUsers.getDate();
-//
-//        for (ProductsItem productsItem:actualUsers.getProducts()){
-//            int productId = productsItem.getProductId();
-//            int quantity = productsItem.getQuantity();
-//        }
+        int actualUserId=actualUsers.getUserId();
+        String actualDate = actualUsers.getDate();
+
+        for (ProductsItem productsItem:actualUsers.getProducts()){
+            int actualProductId = productsItem.getProductId();
+            int actualQuantity = productsItem.getQuantity();
+        }
+
+
         int expectedId= expectedUsers.getId();
+        int expectedUserId= expectedUsers.getUserId();
+        String expectedDate =expectedUsers.getDate();
 
+        for (ProductsItem productsItem:expectedUsers.getProducts()){
+            int expectedProductId = productsItem.getProductId();
+            int expectedQuantity = productsItem.getQuantity();
+        }
 
-        //BaseAssertionLogger.logAndAssert(actualId,expectedId,"pass","fail");
+        BaseAssertionLogger.logAndAssert(expectedId,is(actualId),"Pass","Fail","ID");
+        BaseAssertionLogger.logAndAssert(expectedUserId,is(actualUserId),"Pass","Fail","User ID");
+        BaseAssertionLogger.logAndAssert(expectedDate,is(actualDate),"Pass","Fail","Date");
+        //BaseAssertionLogger.logAndAssert(expectedProductId,is(actualProductId),"Pass","Fail","Date");
 
     }
 
