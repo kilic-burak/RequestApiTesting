169ad0cb261b4ecc9328adb4f4761e23f44044a7
kilic-burak <burak-kilicc@outlook.com.tr>
2024-12-06 13:59:15 +0300

    ValidatorUsers updated

--- src/test/java/helper/ValidatorUsers.java
+++ src/test/java/helper/ValidatorUsers.java
@@ -4,6 +4,8 @@ import io.restassured.response.Response;
 import org.example.models.ProductsItem;
 import org.example.models.ResponseItem;
 
+import java.util.ArrayList;
+
 import static org.hamcrest.Matchers.is;
 
 public class ValidatorUsers {
@@ -16,29 +18,48 @@ public class ValidatorUsers {
     }
     public static void validateUsers(ResponseItem actualUsers, ResponseItem expectedUsers){
 
+        ArrayList<Integer> actualProductId = new ArrayList<>();
+        ArrayList<Integer> actualQuantity=new ArrayList<>();
         int actualId= actualUsers.getId();
         int actualUserId=actualUsers.getUserId();
         String actualDate = actualUsers.getDate();
 
         for (ProductsItem productsItem:actualUsers.getProducts()){
-            int actualProductId = productsItem.getProductId();
-            int actualQuantity = productsItem.getQuantity();
+            actualProductId.add(productsItem.getProductId());
+            actualQuantity.add(productsItem.getQuantity());
+
         }
 
-
+        ArrayList<Integer> expectedProductId = new ArrayList<>();
+        ArrayList<Integer> expectedQuantity=new ArrayList<>();
         int expectedId= expectedUsers.getId();
         int expectedUserId= expectedUsers.getUserId();
         String expectedDate =expectedUsers.getDate();
 
         for (ProductsItem productsItem:expectedUsers.getProducts()){
-            int expectedProductId = productsItem.getProductId();
-            int expectedQuantity = productsItem.getQuantity();
+            expectedProductId.add(productsItem.getProductId());
+            expectedQuantity.add(productsItem.getQuantity());
         }
 
-        BaseAssertionLogger.logAndAssert(expectedId,is(actualId),"Pass","Fail","ID");
-        BaseAssertionLogger.logAndAssert(expectedUserId,is(actualUserId),"Pass","Fail","User ID");
-        BaseAssertionLogger.logAndAssert(expectedDate,is(actualDate),"Pass","Fail","Date");
-        //BaseAssertionLogger.logAndAssert(expectedProductId,is(actualProductId),"Pass","Fail","Date");
+        BaseAssertionLogger.logAndAssert(actualId,is(expectedId),"Pass","Fail","ID");
+        BaseAssertionLogger.logAndAssert(actualUserId,is(expectedUserId),"Pass","Fail","User ID");
+        BaseAssertionLogger.logAndAssert(actualDate,is(expectedDate),"Pass","Fail","Date");
+
+        if (actualProductId.size()==expectedProductId.size()) {
+            for (int i = 0; i < actualProductId.size(); i++) {
+                BaseAssertionLogger.logAndAssert(actualProductId.get(i),is(expectedProductId.get(i)),"Pass","Fail","ProductId "+(i+1));
+            }
+        }
+        else System.out.println("Quantity listeleri birbirlerine eşit değidir.");
+
+        if (actualQuantity.size()==expectedQuantity.size()) {
+            for (int i = 0; i < actualQuantity.size(); i++) {
+                BaseAssertionLogger.logAndAssert(actualQuantity.get(i),is(expectedQuantity.get(i)),"Pass","Fail","Quantity "+(i+1));
+            }
+        }
+        else System.out.println("ProductId listeleri birbirlerine eşit değidir.");
+
+
 
     }
 
